d975072ace81647504df787996e0f304e94fd1d0
The Depressed One <47366052+coolbossinator@users.noreply.github.com>
2021-07-01 16:17:31 -0400

    Add files via upload

--- commands/adminhelp.js
+++ commands/adminhelp.js
@@ -21,8 +21,10 @@ module.exports = {
               .setAuthor("Help!")
               .setTitle(`Commands: `)
               .addFields(
-                { name: '👔 - Administrative Command', value: "`kick - kicks a user`, `ban - bans a user`, `dbb <BAN ID> - shows information on a ban`, `dbk <KICK ID> - shows information on a kick`, `analyze-message <MESSAGE TO ANALYZE> - uses the power of artifical intelligence to see if a message is 'toxic'`" },
-                { name: '🎟️ - Ticket Controls', value: "`ticketcontrols - shows controls to a ticket but be careful it can delete channels!`, `ticketstatus <TICKET ID> - shows the status on a ticket along with more info`, `ticketconfig <CATEGORY ID> - changes what category the tickets get created in`" },
+                { name: '👔 - Administrative Command', value: "`kick - kicks a user`, `ban - bans a user`, `dbb <BAN ID> - shows information on a ban`, `dbk <KICK ID> - shows information on a kick`, `analyze-message <MESSAGE TO ANALYZE> - uses the power of artifical intelligence to see if a message is 'toxic', `automod - to pull up the automod help page`" },
+                { name: '🎟️ - Ticket Controls', value: "`ticketcontrols - shows controls to a ticket but be careful it can delete channels!`, `ticketstatus <TICKET ID> - shows the status on a ticket along with more info`" },
+                { name: '⚙️ - configuration', value: "`logchannel <TAG CHANNEL> - Sets the channel for logs!`, `ticketconfig <CATEGORY ID> - changes what category the tickets get created in`" },
+
                 { name: '\u200b', value: '[Support Me!](https://www.paypal.com/paypalme/infinitydevtech) | [Vote for the bot!](https://top.gg/bot/855035553916518401/vote) | [Bot discord!](https://discord.gg/k4fb9TZfcK)'},
               )
             message.author.send(embed)
