ec69ada9af9fbaef23ce3dba397eaacfce08ad9d
The Depressed One <47366052+coolbossinator@users.noreply.github.com>
2021-07-07 18:57:20 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const { ShardingManager } = require('discord.js');
-const manager = new ShardingManager('./bot.js', { token: 'ODU4NDIyOTQ0MzAwMDA3NDc0.YNd6qg.fdWKWnTXrpdQPY9G7947h1JNpic' });
+const manager = new ShardingManager('./bot.js', { token: 'ODU1MDM1NTUzOTE2NTE4NDAx.YMsn6Q.1G3CEMbbQTkeEBRxv6EFoUsYxEg' });
 
 manager.on('shardCreate', shard => console.log(`Launched shard ${shard.id}`));
 manager.on('shardDeath', shard => console.log(`${shard.id} has died!`))
